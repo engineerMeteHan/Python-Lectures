451b7547ce4d70b5af1e385da74a06b7d452b8af
engineerMeteHan <metehangencereem@example.com>
2025-11-09 23:25:34 +0300

    Final commit for today.

--- main.py
+++ main.py
@@ -233,14 +233,30 @@ arr2 = [4,5,7,9]
 M = len(arr1)
 N = len(arr2)
 findMissing(arr1, arr2, M, N)
+print("-----------------------------------------------------")
 
+def isHeterogram(s, n):
+    hash = [0] * 26 
 
+    # traversing the string 
+    for i in range(n):
 
+        # ignore the space 
+        if s[i] != ' ':
+ 
+            # if already encountered 
+            if hash[ord(s[i]) - ord('a')] == 0:
+                hash[ord(s[i]) - ord('a')] = 1
 
+            # else return false
+            else: 
+                return False
+    return True
 
-
-
-
+# Driven Code   
+s = "the big dwarf only jumpss"
+n = len(s)
+print("YES" if isHeterogram(s,n) else "NO")
 
 
 
