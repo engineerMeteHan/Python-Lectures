6dd2975a4848d882f85489864d31fde57dd15a3c
engineerMeteHan <metehangencereem@example.com>
2025-11-09 21:14:52 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,50 @@
+print("mete")
+print("MEtehan gencer")
+
+c1 = complex(3,4)
+c2 = complex(5)
+c3 = complex()
+
+print(c1)
+print(c2)
+print(c3)
+print(type(c1))
+print(c1.real)
+print(c1.imag)
+print("-------------------------------------------------------")
+print(complex())
+
+
+c1 = complex(3,4)
+print(c1)
+
+c2 = complex(2.5, 3.7)
+print(c2)
+
+c1 = complex("5.5")
+print(c1)
+
+c2 = complex("-2")
+print(c2)
+
+c3 = complex("3+4j")
+print(c3)
+
+c1 = complex(4, 3)
+c2 = complex(2, -2)
+
+print(c1 + c2)
+print(c1 - c2)
+print(c1 * c2)
+print(c1 / c2)  
+
+# accessing real and imaginary parts
+print(c1.real)
+print(c1.imag)
+
+d1 = {1: "mete", 2: "gencer"}
+print(d1)
+
+# Create dictionary using dict() constructor    
+d2 = dict(a = "geeks", b = "nete", c = "metehan")
+print(d2)
